abd902b0f312bcd312c7574bd5e12e448936d274
Amine <amineprojet7@gmail.com>
2021-05-23 23:13:48 +0200

    Initial commit

---
+++ src/pages/home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center content-center justify-center my-80 md:flex-shrink-0">
+      <h1 className="text-gray-700 xl:text-4xl sm:text-xl extrabold">TailwindCSS + React Template</h1>
+      <button className="p-2 text-white transition duration-300 bg-blue-400 rounded-full xl:text-xl sm:text-base hover:bg-blue-600"><a href="https://tailwindcss.com/docs" target="_blank" rel="noreferrer">Documentation</a></button>
+    </div>
+  );
+}
+export default Home;

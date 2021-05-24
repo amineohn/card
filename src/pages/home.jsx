75598e347031ad825ebf7a1d59ef598b9a0df572
Amine <imveny>
2021-05-24 20:59:44 +0200

    add figma icon and replace vscode to figma.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,7 +6,7 @@ import Spotify from "../components/icons/Spotify";
 import React from "../components/icons/React";
 import Twitter from "../components/icons/Twitter";
 import Tailwind from "../components/icons/Tailwind";
-import Code from "../components/icons/Code";
+import Figma from "../components/icons/Figma";
 import Playing from "../components/currentlyPlaying";
 import Profile from "../assets/img/name.gif";
 import { motion } from 'framer-motion';
@@ -29,7 +29,13 @@ const Home = () => {
         <div className="flex flex-col items-center content-center justify-center p-10 mx-auto space-y-3 rounded-lg shadow-xl md:w-1/4 sm:w-2/4 2xl:w-1/5 filter brightness-100 md:flex-shrink-0">
             <img className="pt-1 rounded-full w-36 h-36" src={Profile} alt="" srcset="" />
               <p className="text-lg font-bold text-gray-100">Veny</p> <span className="flex text-sm font-normal text-gray-50">Developer</span>
-              <span className="flex text-sm font-normal text-gray-50"><a className="transition duration-300 transform hover:-translate-y-1 hover:scale-110" href="https://code.visualstudio.com" target="_blank" rel="noreferrer"><Code /></a> <span className="spacement">~</span> <a className="transition duration-300 transform hover:-translate-y-1 hover:scale-110" href="https://fr.reactjs.org" target="_blank" rel="noreferrer"><React /></a> <span className="spacement">~</span> <a className="transition duration-300 transform hover:-translate-y-1 hover:scale-110" href="https://tailwindcss.com" target="_blank" rel="noreferrer"><Tailwind /></a></span>
+              <span className="flex text-sm font-normal text-gray-50">
+                <a className="transition duration-300 transform hover:-translate-y-1 hover:scale-110" href="https://www.figma.com" target="_blank" rel="noreferrer"><Figma /></a> 
+                <span className="spacement">~</span> 
+                <a className="transition duration-300 transform hover:-translate-y-1 hover:scale-110" href="https://fr.reactjs.org" target="_blank" rel="noreferrer"><React /></a> 
+                <span className="spacement">~</span> 
+                <a className="transition duration-300 transform hover:-translate-y-1 hover:scale-110" href="https://tailwindcss.com" target="_blank" rel="noreferrer"><Tailwind /></a>
+              </span>
               <button className="p-2 text-white transition duration-300 transform rounded-lg shadow-xl bg-gradient-to-r from-fuchsia-500 via-red-600 to-orange-400 filter brightness-100 text-normal hover:-translate-y-1 hover:scale-110"><a href="https://github.com/imveny" target="_blank" rel="noreferrer" className="flex">Github<Github /></a></button>
               <button className="p-2 text-white transition duration-300 transform rounded-lg shadow-xl bg-gradient-to-r from-fuchsia-500 via-red-600 to-orange-400 filter brightness-100 text-normal hover:-translate-y-1 hover:scale-110"><a href="#" target="_blank" rel="noreferrer" className="flex">Instagram<Instagram /></a></button>
               <button className="p-2 text-white transition duration-300 transform rounded-lg shadow-xl bg-gradient-to-r from-fuchsia-500 via-red-600 to-orange-400 filter brightness-100 text-normal hover:-translate-y-1 hover:scale-110"><a href="https://discord.gg/PpaxcpcFkd" target="_blank" rel="noreferrer" className="flex">Discord<Discord /></a></button>

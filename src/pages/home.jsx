3810a310460af9dac5143072f85851ccd6ffa8cd
Amine <imveny>
2021-05-24 14:56:28 +0200

    add tailwind icon add twitter and made clickable link.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,6 +4,9 @@ import Github from "../components/icons/Github";
 import Instagram from "../components/icons/Instagram";
 import Spotify from "../components/icons/Spotify";
 import React from "../components/icons/React";
+import Twitter from "../components/icons/Twitter";
+import Tailwind from "../components/icons/Tailwind";
+import Code from "../components/icons/Code";
 import Playing from "../components/currentlyPlaying";
 import Profile from "../assets/img/name.gif";
 import { motion } from 'framer-motion';
@@ -14,7 +17,6 @@ const Home = () => {
     axios.get("https://api.lanyard.rest/v1/users/762055588762877973")
     .then(res => {
       setResponseData(res.data)
-      //console.log(res.data)
       return;
     })
      .catch((error) => {
@@ -27,14 +29,15 @@ const Home = () => {
       animate={{ opacity: 1 }}
       transition={{ duration: 0.55 }}
     >
-      <div className="flex items-center justify-center my-40 xl:my-48 2xl:my-60">
+      <div className="flex items-center justify-center my-40 xl:my-32 2xl:my-60">
         <div className="flex flex-col items-center content-center justify-center p-10 mx-auto space-y-3 rounded-lg shadow-xl md:w-1/4 sm:w-2/4 2xl:w-1/5 filter brightness-100 md:flex-shrink-0">
             <img className="pt-1 rounded-full w-36 h-36" src={Profile} alt="" srcset="" />
-              <p className="text-lg font-bold text-gray-100">Veny</p>
-              <span className="flex text-sm font-normal text-gray-50">Developer <React /></span>
+              <p className="text-lg font-bold text-gray-100">Veny</p> <span className="flex text-sm font-normal text-gray-50">Developer</span>
+              <span className="flex text-sm font-normal text-gray-50"><a className="transition duration-300 transform hover:-translate-y-1 hover:scale-110" href="https://code.visualstudio.com" target="_blank" rel="noreferrer"><Code /></a> <span className="spacement">~</span> <a className="transition duration-300 transform hover:-translate-y-1 hover:scale-110" href="https://fr.reactjs.org" target="_blank" rel="noreferrer"><React /></a> <span className="spacement">~</span> <a className="transition duration-300 transform hover:-translate-y-1 hover:scale-110" href="https://tailwindcss.com" target="_blank" rel="noreferrer"><Tailwind /></a></span>
               <button className="p-2 text-white transition duration-300 transform rounded-lg bg-gradient-to-r from-blue-gray-900 via-purple-900 to-blue-gray-900 text-normal hover:-translate-y-1 hover:scale-110"><a href="https://github.com/imveny" target="_blank" rel="noreferrer" className="flex">Github<Github /></a></button>
               <button className="p-2 text-white transition duration-300 transform rounded-lg bg-gradient-to-r from-blue-gray-900 via-purple-900 to-blue-gray-900 text-normal hover:-translate-y-1 hover:scale-110"><a href="#" target="_blank" rel="noreferrer" className="flex">Instagram<Instagram /></a></button>
               <button className="p-2 text-white transition duration-300 transform rounded-lg bg-gradient-to-r from-blue-gray-900 via-purple-900 to-blue-gray-900 text-normal hover:-translate-y-1 hover:scale-110"><a href="https://discord.gg/PpaxcpcFkd" target="_blank" rel="noreferrer" className="flex">Discord<Discord /></a></button>
+              <button className="p-2 text-white transition duration-300 transform rounded-lg bg-gradient-to-r from-blue-gray-900 via-purple-900 to-blue-gray-900 text-normal hover:-translate-y-1 hover:scale-110"><a href="https://twitter.com/fuckthisimoutd" target="_blank" rel="noreferrer" className="flex">Twitter<Twitter /></a></button>
               <div className="flex items-center justify-center">
                 <Spotify /> 
                 <Playing />
